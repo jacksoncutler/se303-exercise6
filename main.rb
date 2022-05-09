143c3ea6f41182ab1199e8b8edc27c40c21fd853
Jackson <jackson.cutler@gmail.com>
2022-05-08 23:36:34 -0700

    Consolidate call instantiation of Button and call to draw

--- main.rb
+++ main.rb
@@ -28,8 +28,7 @@ def draw_button(label_text, x, y, foreground_color, is_dark_mode)
   if is_dark_mode
     Button.new(label_text, x, y, foreground_color, is_dark_mode).draw
   else
-    button = Button.new(label_text, x, y, foreground_color, is_dark_mode)
-    button.draw
+    Button.new(label_text, x, y, foreground_color, is_dark_mode).draw
   end
 end
 
