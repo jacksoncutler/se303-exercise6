e2f6662cadf761823e1c13c80070160e6ff4cc6d
Jackson <jackson.cutler@gmail.com>
2022-05-08 23:34:41 -0700

    Replace paint with call to draw method

--- main.rb
+++ main.rb
@@ -27,7 +27,7 @@ end
 def draw_button(label_text, x, y, foreground_color, is_dark_mode)
   if is_dark_mode
     button = Button.new(label_text, x, y, foreground_color, is_dark_mode)
-    paint(label_text, x, y, darken_foreground(foreground_color), '#111111')
+    button.draw
   else
     paint(label_text, x, y, lighten_foreground(foreground_color), '#E0E0E0')
   end
