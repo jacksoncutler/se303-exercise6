f2798e2a6606d6a318faa716e28459cd3aff1d43
Jackson <jackson.cutler@gmail.com>
2022-05-08 23:39:18 -0700

    Simplify button darken and lighten method names

--- main.rb
+++ main.rb
@@ -1,10 +1,9 @@
 class Button
-
   def initialize(label, x, y, foreground_color, is_dark_mode)
     @label = label
     @x = x
     @y = y
-    @foreground = is_dark_mode ? darken_foreground(foreground_color) : lighten_foreground(foreground_color)
+    @foreground = is_dark_mode ? darken(foreground_color) : lighten(foreground_color)
     @secondary is_dark_mode ? '#111111' : '#E0E0E0'
   end
 
@@ -14,14 +13,13 @@ class Button
 
  private
 
-  def darken_foreground(color)
+  def darken(color)
     color - 10
   end
   
-  def lighten_foreground(color)
+  def lighten(color)
     color + 10
   end
-  
 end
 
 def draw_button(label_text, x, y, foreground_color, is_dark_mode)
